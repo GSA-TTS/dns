41e17891542d6e881dddb202dc433616c1c13cee
Mark Boyd <mark.boyd@gsa.gov>
2025-03-24 13:52:21 -0400

    revert change to CNAME for tock.18f.gov

--- terraform/tock.18f.gov.tf
+++ terraform/tock.18f.gov.tf
@@ -7,11 +7,10 @@ resource "aws_route53_record" "tock_18f_gov_acmechallenge" {
   records = ["_acme-challenge.tock.18f.gov.external-domains-production.cloud.gov."]
 }
 
-resource "aws_route53_record" "tock_18f_gov_cname" {
+resource "aws_route53_record" "d_18f_gov_tock_18f_gov_cname" {
   zone_id = aws_route53_zone.d_18f_gov_zone.zone_id
   name    = "tock.18f.gov."
   type    = "CNAME"
-
-  ttl     = 600
-  records = ["tock.18f.gov.external-domains-production.cloud.gov."]
+  ttl     = 300
+  records = ["production-domains-1-884689640.us-gov-west-1.elb.amazonaws.com."]
 }
